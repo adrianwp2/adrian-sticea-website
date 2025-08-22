ae8af6ea57df49d05e20d43dcad694cd75e38320
adrianwp2 <85935522+adrianwp2@users.noreply.github.com>
2025-08-22 19:12:52 -0400

    changing title and desc

--- app/layout.js
+++ app/layout.js
@@ -4,24 +4,24 @@ import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 
 const InterSans = Inter({
-    variable: "--font-inter",
-    subsets: ["latin"],
+  variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 export const metadata = {
-    title: "Building in Public to Become a Better Engineer",
-    description:
-        " I'm Adrian — a developer learning by building real products. Follow along, see what I'm working on.",
+  title: "Full-Stack Developer Ready to Build",
+  description:
+    "Experienced developer who transforms ideas into powerful digital solutions. From custom websites to SaaS applications, I deliver results that drive business growth.",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className="antialiased">
-                {children}
-                <Toaster position="	top-center" />
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        {children}
+        <Toaster position="	top-center" />
+        <Footer />
+      </body>
+    </html>
+  );
 }
